218aac16f8d9601b71438043bf2b235d1dd1aea9
kovsemy <iiinflame@yandex.ru>
2025-07-23 13:53:07 +0300

    minor fix

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -1,10 +1,8 @@
-package Lesson2;
+package lesson2;
 
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Lesson2 {
+public class Main {
     public static void main(String[] args) {
         printThreeWords(); // 1
         checkSumSign(); // 2
@@ -25,7 +23,7 @@ public class Lesson2 {
     /**
     1. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple
      */
-    static void printThreeWords() {
+    public static void printThreeWords() {
         System.out.println("Orange" + "\n" + "Banana" + "\n" + "Apple");
     }
 
@@ -34,7 +32,7 @@ public class Lesson2 {
     значениями, которыми захотите. Далее метод должен просуммировать эти переменные, и если их сумма больше или равна 0,
     то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
      */
-    static void checkSumSign() {
+    public static void checkSumSign() {
         int a = 0;
         int b = 1;
         if (a + b >= 0) {
@@ -49,7 +47,7 @@ public class Lesson2 {
     Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”, если лежит в пределах
     от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”, если больше 100 (100 исключительно) - “Зеленый”;
      */
-    static void printColor() {
+    public static void printColor() {
         int value = 0;
         if (value <= 0) {
             System.out.println("Красный");
@@ -65,7 +63,7 @@ public class Lesson2 {
     значениями, которыми захотите. Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”,
     в противном случае “a < b”;
      */
-    static void compareNumbers() {
+    public static void compareNumbers() {
         int a = 0;
         int b = 1;
         if (a >= b) {
@@ -79,7 +77,7 @@ public class Lesson2 {
     5. Напишите метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах
     от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
      */
-    static boolean isSumInRange(int a, int b) {
+    public static boolean isSumInRange(int a, int b) {
         int sum = a + b;
         if (sum >= 10 && sum <= 20) {
            return true;
@@ -92,7 +90,7 @@ public class Lesson2 {
     метод должен напечатать в консоль, положительное ли число передали или отрицательное.
     Замечание: ноль считаем положительным числом.
     */
-    static void printIsPositive(int a) {
+    public static void printIsPositive(int a) {
         if (a >= 0) {
             System.out.println(a + " положительное число");
         } else {
@@ -105,7 +103,7 @@ public class Lesson2 {
     Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
     Замечание: ноль считаем положительным числом.
      */
-    static boolean isNegative(int a) {
+    public static boolean isNegative(int a) {
         return a < 0;
     }
 
@@ -113,7 +111,7 @@ public class Lesson2 {
     8. Напишите метод, которому в качестве аргументов передается строка и число,
     метод должен отпечатать в консоль указанную строку, указанное количество раз.
      */
-    static void repeatPrintString(String str, int count) {
+    public static void repeatPrintString(String str, int count) {
         for (int i = 0; i < count; i++) {
             System.out.println(str);
         }
@@ -124,7 +122,7 @@ public class Lesson2 {
     и возвращает boolean (високосный - true, не високосный - false).
     Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
      */
-    static boolean isLeapYear(int year) {
+    public static boolean isLeapYear(int year) {
         if (year % 4 == 0) {
             if (year % 100 == 0) {
                 return year % 400 == 0;
@@ -139,7 +137,7 @@ public class Lesson2 {
     10. Задать целочисленный массив, состоящий из элементов 0 и 1.
     Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0.
      */
-    static void printFlipArray() {
+    public static void printFlipArray() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
@@ -155,7 +153,7 @@ public class Lesson2 {
     11. Задать пустой целочисленный массив длиной 100.
     С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100.
      */
-    static void fillEmptyArray() {
+    public static void fillEmptyArray() {
         int[] arr = new int[100];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
@@ -166,7 +164,7 @@ public class Lesson2 {
     /**
     12. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2.
      */
-    static void multiplyArrayValues() {
+    public static void multiplyArrayValues() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 6) {
@@ -182,10 +180,10 @@ public class Lesson2 {
     Определить элементы одной из диагоналей можно по следующему принципу:
     индексы таких элементов равны, то есть [0][0], [1][1], [2][2], ..., [n][n];
      */
-    static void drawArray() {
+    public static void drawArray() {
         int[][] arr = new int[3][3];
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = 0; j < arr[i].length; j++) {
                 if (j == i || j + i == arr.length - 1) {
                     arr[i][j] = 1;
                     System.out.print("1 ");
@@ -202,7 +200,7 @@ public class Lesson2 {
     14. Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий
     одномерный массив типа int длиной len, каждая ячейка которого равна initialValue.
      */
-    static int[] getNewArray(int len, int initialValue) {
+    public static int[] getNewArray(int len, int initialValue) {
         int[] newArray = new int[len];
         Arrays.fill(newArray, initialValue);
         System.out.println(Arrays.toString(newArray));
